5948843892a3597c4ca2ea7a300c49f6e2275532
Kevin Malan <kevinm@wetu.com>
2025-05-02 18:47:20 +0200

    Added test for password hash and verify

--- Tests/Auth/PasswordTests.cs
+++ Tests/Auth/PasswordTests.cs
@@ -43,5 +43,35 @@ namespace Tests.Auth
             // Assert
             result.ShouldBe(expected);
         }
+
+        [Theory]
+        [InlineData("a", "b", false)]
+        [InlineData("111", "11", false)]
+        [InlineData("V@lidP@ssword15", "V@lidP@ssword14", false)]
+        [InlineData("V@lidP@ssword15", "V@lidP@ssword15", true)]
+        public void Hash_Verify_ShouldMatch(string password1, string password2, bool expectedResult)
+        {
+            // Arrange
+            var options = Substitute.For<IOptions<PasswordConfig>>();
+            options.Value.Returns(new PasswordConfig
+            {
+                SaltLength = 16,
+                HashLength = 32,
+                MemoryKb = 65536,
+                Iterations = 4,
+                Parallelism = 2,
+                Pepper = $"{Guid.NewGuid()}"
+            });
+            var service = new PasswordService(options);
+
+            // Act
+            var storedPassword = service.HashPassword(password1);
+            var storedPasswordHash = storedPassword.Hash;
+            var storedSaltHash = storedPassword.Salt;
+            var verifiedResult = service.Verify(password2, storedPasswordHash, storedSaltHash);
+
+            // Assert
+            verifiedResult.ShouldBe(expectedResult);
+        }
     }
 }
